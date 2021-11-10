744963729ddfcd28c0b70165afb0e74098feb896
Julia Lana Patto <julialanapatto@gmail.com>
2021-11-10 09:15:18 -0300

    Add exercises

---
+++ Fundamentals/04.Intro-javascript/day-3-programming-logic-and-algorithms/2.print-triangle-asterisks.js
@@ -0,0 +1,10 @@
+//2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. 
+// 2- For the second exercise, do the same as before, but print a right triangle with 5 base asterisks.
+
+let n = 5;
+let asteriks = "*";
+let line=""
+for (i=0; i<n; i+=1){
+    line = line + asteriks;
+    console.log(line)
+}
