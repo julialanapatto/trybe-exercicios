7989444739a6ed9e1c6588a64511c4e882da2de2
Julia Lana Patto <julialanapatto@gmail.com>
2021-12-08 16:49:37 -0300

    part two

---
+++ Fundamentals/07.Javascript-ES6/day-1-Javascript-let-const-arrow-functions-template-literals/part-2-fixation.js
@@ -0,0 +1,17 @@
+/* const myName = "Isabella"
+console.log('Hello' + ' ' + myName + '!');
+
+const myName = "Isabella"
+console.log(`Welcome ${myName}!`); */
+
+/* const test = "primeiralinha, segundalinha, terceiralinha"
+console.log (`concatena ${test}`); */
+
+// Com o template literals
+console.log(`Primeira linha;
+Segunda linha;
+Terceira linha;`
+)
+
+// Sem o template literals:
+console.log('Primeira linha;\n' + 'Segunda linha;\n' + 'Terceira linha;\n')
