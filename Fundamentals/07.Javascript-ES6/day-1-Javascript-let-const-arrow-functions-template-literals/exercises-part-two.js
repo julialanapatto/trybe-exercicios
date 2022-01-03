36363868ad2f45cdb1766e15a48cefd40dea492a
Julia Lana Patto <julialanapatto@gmail.com>
2022-01-03 14:58:46 -0300

    exercises

---
+++ Fundamentals/07.Javascript-ES6/day-1-Javascript-let-const-arrow-functions-template-literals/exercises-part-two.js
@@ -0,0 +1,38 @@
+// Crie uma função que receba um número e retorne seu fatorial.
+/* const fatorial = number => {
+    let result = 1;
+    for (let index = 2; index <= number; index += 1) {
+        result *= index;
+    }
+    return result
+}
+
+console.log(fatorial(6));
+ */
+// Crie uma função que receba uma frase e retorne qual a maior palavra.
+
+/* const maiorPalavra = frase => {
+    let separaPalavra = frase.split(' ') //palavras como novas strings
+    let contadorPalavras = 0 //começa com zero
+    let resultado = ''; // começa com array vazio
+
+    for(const palavra of separaPalavra) {
+        if (palavra.length > contadorPalavras) {
+            contadorPalavras = palavra.length
+            resultado = palavra
+        }
+    }
+    return resultado
+}
+console.log(maiorPalavra("Qual será a maior palavra dessa frase?"))
+ */
+
+//
+//acumula o numero de cliques do botao
+/* let text = document.getElementById("text");
+let button = document.getElementById("button")
+let clickCount = 0;
+
+button.addEventListener('click', () => text.innerText = clickCount += 1); */
+
+
